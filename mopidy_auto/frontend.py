d7e8d604b0d27779682133be64c7b8d78413a49d
Marcus Gotling <marcus@gotling.se>
2017-03-23 13:09:34 +0800

    Play a random album

--- mopidy_auto/frontend.py
+++ mopidy_auto/frontend.py
@@ -1,3 +1,5 @@
+import random
+
 import pykka
 
 from mopidy import core
@@ -11,28 +13,40 @@ class AutoFrontend(pykka.ThreadingActor, core.CoreListener):
         self.config = config
         self.core = core
 
-    def get_random_album(self):
-
-        tracks = self.get_tracks(base_path + 'Rock')
-        print("\n----\nFound {} tracks\n----\n".format(len(tracks)))
-        uris = []
-        for track in tracks:
-            uris.append(track.uri)
-            print(track.name)
-
-        self.core.tracklist.clear()
-        self.core.tracklist.add(uris=uris)
-        self.core.playback.play()
-
     def track_playback_ended(self, tl_track, time_position):
         if self.core.tracklist.index(tl_track).get() == self.core.tracklist.get_length().get() - 1:
-            self.get_random_album()
+            uri = base_path + 'Rock'
+            tracks = self.get_random_album(uri)
+            self.play_uris(tracks)
 
-    def get_tracks(self, uri):
+    def get_random_album(self, uri):
+        track_uris = []
         print(uri)
+
+        refs = self.core.library.browse(uri).get()
+
+        for ref in refs:
+            if ref.type == 'track':
+                track_uris.append(ref.uri)
+
+        if len(track_uris) > 0:
+            return track_uris
+
+        rand_idx = random.randint(0, len(refs) - 1)
+        return self.get_random_album(refs[rand_idx].uri)
+
+    """
+    def get_tracks(self, uri):
         results = self.core.library.browse(uri).get()
         for result in results:
             if result.type == 'directory':
                 return self.get_tracks(result.uri)
 
         return results
+    """
+    def play_uris(self, uris):
+        print("\n----\nFound {} tracks\n----\n".format(len(uris)))
+
+        self.core.tracklist.clear()
+        self.core.tracklist.add(uris=uris)
+        self.core.playback.play()
