2411633e0f89102ff558452e90a542d895b92cb5
Marcus Gotling <marcus@gotling.se>
2017-03-23 14:58:41 +0800

    Simple user interface

---
+++ mopidy_auto/static/js/auto.js
@@ -0,0 +1,80 @@
+var basePath = 'file:///Users/marcus/Media/Music/';
+
+var mopidy = new Mopidy();
+
+/*
+** BINDING
+**/
+
+mopidy.on(console.log.bind(console));
+
+mopidy.on("state:online", function () {
+   mopidy.playback.getState().done(function(state) {
+      toggleButtons(state);
+   });
+   mopidy.playback.getCurrentTrack().done(function(track) {
+      if (track) {
+         updateCurrentTrack(track);
+      }
+   });
+   mopidy.playback.getVolume().done(function(volume) {
+      setVolumeUi(volume);
+   });
+});
+
+$('#play').on('click', function() {
+   mopidy.playback.play();
+});
+
+$('#pause').on('click', function() {
+   mopidy.playback.pause();
+});
+
+$('#next').on('click', function() {
+   mopidy.playback.next();
+});
+
+$('#volume').on('change', function() {
+   var volume = parseInt($(this).val());
+   mopidy.playback.setVolume(volume);
+});
+
+/*
+** EVENTS
+**/
+
+mopidy.on('event:playbackStateChanged', function(event) {
+   toggleButtons(event.new_state);
+});
+
+mopidy.on("event:trackPlaybackStarted", function(event) {
+   updateCurrentTrack(event.tl_track.track);
+});
+
+mopidy.on('event:volumeChanged', function(event) {
+   setVolumeUi(event.volume);
+});
+
+/*
+** FUNCTIONS
+**/
+
+function updateCurrentTrack(track) {
+   $('#current-track').text(track.name);
+   $('#current-artist').text(track.artists[0].name);
+   $('#current-album').text(track.album.name);
+}
+
+function setVolumeUi(volume) {
+   $('#volume').val(volume);
+}
+
+function toggleButtons(state) {
+   if (state === 'playing') {
+      $('#play').hide();
+      $('#pause').show();
+   } else {
+      $('#play').show();
+      $('#pause').hide();
+   }
+}
