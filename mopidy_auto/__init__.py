2d1ea23720358bbdeb395372167e5d425b3d6fef
Marcus Gotling <marcus@gotling.se>
2017-03-26 20:14:53 +0800

    Cleanup

--- mopidy_auto/__init__.py
+++ mopidy_auto/__init__.py
@@ -11,7 +11,6 @@ from .web import IndexHandler, VolumeHandler
 
 __version__ = '0.1.0'
 
-# TODO: If you need to log, use loggers named after the current Python module
 logger = logging.getLogger(__name__)
 
 
