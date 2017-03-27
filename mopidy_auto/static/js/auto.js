cb779b94f8a86700baf2245f04cf3cc92cd3bcb0
Marcus Gotling <marcus@gotling.se>
2017-03-27 21:08:45 +0800

    Relates to #9, display time of current track. Needs cleanup

--- mopidy_auto/static/js/auto.js
+++ mopidy_auto/static/js/auto.js
@@ -1,6 +1,13 @@
 var mopidy = new Mopidy();
 
 /*
+** GLOBALS
+**/
+
+var trackPosition = 0;
+var trackPositionTimer;
+
+/*
 ** BINDING
 **/
 
@@ -12,12 +19,20 @@ mopidy.on("state:online", function () {
    mopidy.playback.getState().done(function(state) {
       toggleButtons(state);
    });
+
    // Update track information
    mopidy.playback.getCurrentTrack().done(function(track) {
       if (track) {
          updateCurrentTrack(track);
       }
    });
+
+   // Update track position
+    mopidy.playback.getTimePosition().done(function(timePosition) {
+       trackPosition = timePosition;
+       trackPositionTimer = setInterval(updateCurrentTrackPosition, 1000);
+    });
+
    // Update volume position
    mopidy.playback.getVolume().done(function(volume) {
       setVolumeUi(volume);
@@ -61,14 +76,33 @@ $('#volume').on('change', function() {
 
 mopidy.on('event:playbackStateChanged', function(event) {
    toggleButtons(event.new_state);
+   clearInterval(trackPositionTimer);
 });
 
 mopidy.on("event:trackPlaybackStarted", function(event) {
    updateCurrentTrack(event.tl_track.track);
+   trackPosition = 0;
+   updateCurrentTrackPosition();
+   trackPositionTimer = setInterval(updateCurrentTrackPosition, 1000);
 });
 
 mopidy.on('event:volumeChanged', function(event) {
    setVolumeUi(event.volume);
+});
+
+mopidy.on('event:trackPlaybackResumed', function(event) {
+   updateCurrentTrack(event.tl_track.track);
+   trackPosition = event.time_position;
+   trackPositionTimer = setInterval(updateCurrentTrackPosition, 1000);
+});
+
+mopidy.on('event:seeked', function(event) {
+   trackPosition = event.time_position;
+   updateCurrentTrackPosition();
+});
+
+mopidy.on('event:trackPlaybackPaused', function(event) {
+   clearInterval(trackPositionTimer);
 });
 
 /*
@@ -79,6 +113,13 @@ function updateCurrentTrack(track) {
    $('#current-track').text(track.name);
    $('#current-artist').text(track.artists[0].name);
    $('#current-album').text(track.album.name);
+
+   $('#track-length').text(msToTimeString(track.length));
+}
+
+function updateCurrentTrackPosition() {
+   $('#track-position').text(msToTimeString(trackPosition));
+   trackPosition += 1000;
 }
 
 function setVolumeUi(volume) {
@@ -95,4 +136,20 @@ function toggleButtons(state) {
       $('#pause-div').hide();
       $('.next-button').addClass('disabled');
    }
+}
+
+/*
+** HELPERS
+**/
+function msToTimeString(ms) {
+   var seconds = parseInt(ms / 1000);
+   var minutes = parseInt(seconds / 60);
+   seconds = seconds % 60;
+   return minutes + ":" + pad(seconds, 2);
+}
+
+function pad(n, width, z) {
+   z = z || '0';
+   n = n + '';
+   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
