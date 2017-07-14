419cc6d78046cc3ccc16c255d481dae9cf1def1d
Marcus Gotling <marcus@gotling.se>
2017-07-14 09:54:55 +0800

    Fixes #2 and Fixes #3: Delete track and album

--- mopidy_auto/web.py
+++ mopidy_auto/web.py
@@ -1,6 +1,13 @@
 import os
+import shutil
 
+import urllib
+import urlparse
+
+import logging
 import tornado.web
+
+logger = logging.getLogger(__name__)
 
 
 class IndexHandler(tornado.web.RequestHandler):
@@ -13,7 +20,25 @@ class IndexHandler(tornado.web.RequestHandler):
         return self.render('index.html', authed=self.get_secure_cookie('authed', None))
 
     def post(self, path):
-        if self.get_body_argument("password") == '123':
+        action = self.get_body_argument("action", False)
+        if action:
+            if action == 'delete-track':
+                track = self.core.playback.get_current_track().get()
+                if track:
+                    track_path = urllib.url2pathname(urlparse.urlparse(track.uri.split('file://')[1]).path)
+                    self.core.playback.next()
+                    os.unlink(track_path)
+                    logger.info("Deleted track '{}'".format(track.name))
+            elif action == 'delete-album':
+                track = self.core.playback.get_current_track().get()
+                if track:
+                    album_path = os.path.dirname(urllib.url2pathname(urlparse.urlparse(track.uri.split('file://')[1]).path))
+                    self.core.tracklist.clear()
+                    shutil.rmtree(album_path, True)
+                    logger.info("Deleted album '{}'".format(track.album.name))
+            elif action == 'move-album':
+                print('Move current album')
+        elif self.get_body_argument("password") == '123':
             self.set_secure_cookie('authed', 'true')
             return self.render('index.html', authed='true')
 
